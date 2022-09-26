6431774b730445bdfa6def6668a89303694119c8
Arinji2 <87296817+Arinji2@users.noreply.github.com>
2022-09-26 16:12:25 +0530

    Update nav.js

--- src/Components/nav.js
+++ src/Components/nav.js
@@ -1,14 +1,12 @@
 import React, { Component } from "react";
 
-import "./Components.css";
-
 class Nav extends Component {
   state = {};
   render() {
     return (
       <React.Fragment>
         <div className="flex flex-row border-b-white border-b-2 rounded text-2xl pt-4 justify-between sticky top-0 z-50 bg-black-950 ">
-          <p className="pl-5">
+          <p className="pl-5 text-2">
             InfoBot<span className="text-yellow-500">.</span>
           </p>
           <p className="pr-24"></p>
